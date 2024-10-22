c497f3c48c61c0491b8a0fa91d80b276fc4cc1b9
Sergiy Gorbachov <ottolindholmplay@gmail.com>
2024-10-22 17:30:27 +0300

    Add Train & TrainType models to models.py

--- train_station/models.py
+++ train_station/models.py
@@ -7,3 +7,24 @@ class Crew(models.Model):
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
+
+class TrainType(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class Train(models.Model):
+    name = models.CharField(max_length=50)
+    cargo_num = models.IntegerField()
+    places_in_cargo = models.IntegerField()
+    train_type = models.ForeignKey(
+        TrainType,
+        on_delete=models.CASCADE,
+        related_name="train_types"
+    )
+
+    def __str__(self):
+        return f"{self.name} ({self.train_type})"
