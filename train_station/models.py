497a47e8cb17f1961033b716e3b27c6603e7ed3a
Sergiy Gorbachov <ottolindholmplay@gmail.com>
2024-10-22 17:34:19 +0300

    Add Station & Route models to models.py

--- train_station/models.py
+++ train_station/models.py
@@ -21,10 +21,30 @@ class Train(models.Model):
     cargo_num = models.IntegerField()
     places_in_cargo = models.IntegerField()
     train_type = models.ForeignKey(
-        TrainType,
-        on_delete=models.CASCADE,
-        related_name="train_types"
+        TrainType, on_delete=models.CASCADE, related_name="train_types"
     )
 
     def __str__(self):
         return f"{self.name} ({self.train_type})"
+
+
+class Station(models.Model):
+    name = models.CharField(max_length=50)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    def __str__(self):
+        return f"{self.name} ({self.latitude}, {self.longitude})"
+
+
+class Route(models.Model):
+    source = models.ForeignKey(
+        Station, on_delete=models.CASCADE, related_name="routes"
+    )
+    destination = models.ForeignKey(
+        Station, on_delete=models.CASCADE, related_name="routes"
+    )
+    distance = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.source.name}-{self.destination.name}"
