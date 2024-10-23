531496e9c35d4c9be98d6cf12ad6efd02e76dae9
Sergiy Gorbachov <ottolindholmplay@gmail.com>
2024-10-23 13:54:56 +0300

    Add trips field to Crew model, update migrations

--- train_station/models.py
+++ train_station/models.py
@@ -3,18 +3,6 @@ from django.db import models
 from train_station_service import settings
 
 
-class Crew(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-
-    def __str__(self):
-        return f"{self.first_name} {self.last_name}"
-
-    @property
-    def full_name(self):
-        return self.__str__()
-
-
 class TrainType(models.Model):
     name = models.CharField(max_length=50)
 
@@ -68,6 +56,19 @@ class Trip(models.Model):
 
     def __str__(self):
         return f"{self.train.name} ({self.route})"
+
+
+class Crew(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    trips = models.ManyToManyField(Trip)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
+    @property
+    def full_name(self):
+        return self.__str__()
 
 
 class Order(models.Model):
