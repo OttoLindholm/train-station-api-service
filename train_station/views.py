998f935d0c6c6208c2734158392e76b80068f83a
Sergiy Gorbachov <ottolindholmplay@gmail.com>
2024-10-23 20:51:33 +0300

    Add Route view set

--- train_station/views.py
+++ train_station/views.py
@@ -1,18 +1,33 @@
 from rest_framework import viewsets
 
 from train_station.models import (
-    Train, Station,
+    Train,
+    Station,
+    Route,
 )
 from train_station.serializers import (
-    TrainSerializer, StationSerializer,
+    TrainSerializer,
+    StationSerializer,
+    RouteSerializer,
+    RouteListSerializer,
 )
 
 
 class TrainViewSet(viewsets.ModelViewSet):
-    queryset = Train.objects.all()
+    queryset = Train.objects.select_related("train_type").all()
     serializer_class = TrainSerializer
 
 
 class StationViewSet(viewsets.ModelViewSet):
     queryset = Station.objects.all()
     serializer_class = StationSerializer
+
+
+class RouteViewSet(viewsets.ModelViewSet):
+    queryset = Route.objects.select_related("source", "destination").all()
+    serializer_class = RouteSerializer
+
+    def get_serializer_class(self):
+        if self.action == "list":
+            return RouteListSerializer
+        return RouteSerializer
