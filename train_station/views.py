38666ffe11a076df3a4df52b776be731c9faabc0
Sergiy Gorbachov <ottolindholmplay@gmail.com>
2024-10-23 20:35:55 +0300

    Add Train & Station view sets

--- train_station/views.py
+++ train_station/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from train_station.models import (
+    Train, Station,
+)
+from train_station.serializers import (
+    TrainSerializer, StationSerializer,
+)
+
+
+class TrainViewSet(viewsets.ModelViewSet):
+    queryset = Train.objects.all()
+    serializer_class = TrainSerializer
+
+
+class StationViewSet(viewsets.ModelViewSet):
+    queryset = Station.objects.all()
+    serializer_class = StationSerializer
